d51e2a27082b51a50e5820502bacfdbb708c7ac1
Nikita <nikita.yzkii@gmail.com>
2025-12-20 19:12:22 +0300

    доработана загрузка моделей

--- backend/providers/embeddings_provider.py
+++ backend/providers/embeddings_provider.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from langchain_huggingface import HuggingFaceEmbeddings
 from fastembed import SparseTextEmbedding
 from sentence_transformers import SentenceTransformer
-from config import DENSE_MODEL_PATH, SPARSE_MODEL_PATH, SPARSE_MODEL_NAME
+from config import DENSE_MODEL_PATH, DENSE_MODEL_NAME, SPARSE_MODEL_PATH, SPARSE_MODEL_NAME
 
 class EmbeddingsProvider:
     """
@@ -16,10 +16,12 @@ class EmbeddingsProvider:
     def __init__(
         self,
         dense_model_path: str = None,
+        dense_model_name: str = None,
         sparse_model_path: str = None,
         sparse_model_name: str = None,
     ):
         self.dense_model_path = dense_model_path or DENSE_MODEL_PATH or "./huggingface_models/dense"
+        self.dense_model_name = dense_model_name or DENSE_MODEL_NAME or self.DEFAULT_DENSE_MODEL
         self.sparse_model_path = sparse_model_path or SPARSE_MODEL_PATH or "./huggingface_models/sparse"
         self.sparse_model_name = sparse_model_name or SPARSE_MODEL_NAME or self.DEFAULT_SPARSE_MODEL
 
@@ -27,14 +29,16 @@ class EmbeddingsProvider:
         if not Path(self.dense_model_path).exists():
             print(f"Dense model not found at {self.dense_model_path}, downloading...")
             Path(self.dense_model_path).mkdir(parents=True, exist_ok=True)
-            SentenceTransformer(self.DEFAULT_DENSE_MODEL).save(self.dense_model_path)
+            SentenceTransformer(self.dense_model_name).save(self.dense_model_path)
             print("Dense model downloaded.")
 
     def get_dense_embeddings(self):
         return HuggingFaceEmbeddings(
-            model_name=self.dense_model_path,
+            model_name=self.dense_model_name,
+            cache_folder=self.dense_model_path,
             model_kwargs={"device": "cpu", "trust_remote_code": True},
-            encode_kwargs={"normalize_embeddings": True}
+            encode_kwargs={"normalize_embeddings": True},
+            show_progress=True
         )
 
     def get_sparse_embeddings(self):
